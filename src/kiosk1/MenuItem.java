1e857e70e20973848443491e0c42f37186c6397f
minss22 <kms141516@gmail.com>
2025-10-22 12:19:47 +0900

    ✨ feat: MenuItem 클래스 생성
    - 속성 생성: 이름, 가격, 설명
    - 메서드 생성

---
+++ src/kiosk1/MenuItem.java
@@ -0,0 +1,13 @@
+package kiosk1;
+
+public class MenuItem {
+    String name;
+    String price;
+    String info;
+
+    MenuItem(String name, String price, String info) {
+        this.name = name;
+        this.price = price;
+        this.info = info;
+    }
+}
