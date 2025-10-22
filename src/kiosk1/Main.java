08dc93c04efa776e680207b903cddd783c8e37f0
minss22 <kms141516@gmail.com>
2025-10-22 12:49:00 +0900

    ✨ feat: MenuItem 클래스 활용하여 햄버거 메뉴를 출력
    - 객체 생성을 통해 이름, 가격, 설명을 세팅
    - List를 선언하여 여러 MenuItem을 추가
    - 반복문을 활용해 menuItems를 탐색하면서 메뉴 하나씩 출력

--- src/kiosk1/Main.java
+++ src/kiosk1/Main.java
@@ -1,21 +1,32 @@
 package kiosk1;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        List<MenuItem> menuItems = new ArrayList<>();
+        // 햄버거 메뉴 세팅
+        menuItems.add(new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("SmokeShack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
         Scanner sc = new Scanner(System.in);
         int input = -1;
 
         while (input!=0) { // 0 입력 시 종료
-            System.out.println("""
-                    [ SHAKESHACK MENU ]
-                    1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거
-                    2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거
-                    3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거
-                    4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거
-                    0. 종료""");
+            // 햄버거 메뉴 출력
+            System.out.println("[ SHAKESHACK MENU ]");
+            for (int i=0;i<menuItems.size();i++) {
+                MenuItem menu = menuItems.get(i);
+                System.out.printf("%d. %-13s | %4d 원 | %s\n", i+1, menu.name, menu.price, menu.info);
+            }
+            System.out.print("0. 종료\n- 입력: ");
 
-            System.out.println("- 입력: ");
-            input = sc.nextInt(); // ✅ 입력받기
+            input = sc.nextInt(); // 입력받기
+            MenuItem menu = menuItems.get(input-1);
+            System.out.printf("- 선택한 메뉴: %s, %d원 (%s)\n\n", menu.name, menu.price, menu.info);
         }
     }
 }
--- src/kiosk1/MenuItem.java
+++ src/kiosk1/MenuItem.java
@@ -2,10 +2,10 @@ package kiosk1;
 
 public class MenuItem {
     String name;
-    String price;
+    int price;
     String info;
 
-    MenuItem(String name, String price, String info) {
+    MenuItem(String name, int price, String info) {
         this.name = name;
         this.price = price;
         this.info = info;
