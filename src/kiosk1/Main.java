5745603b43691ad179df235d69b784da28bf99ef
minss22 <kms141516@gmail.com>
2025-10-22 11:41:45 +0900

    ✨ feat: 메뉴 출력 및 선택하기
    - Scanner를 사용하여 입력받은 숫자에 따라 다른 로직을 실행
    - 반복문을 이용해서 특정 번호가 입력되면 프로그램을 종료

--- src/kiosk1/Main.java
+++ src/kiosk1/Main.java
@@ -1,12 +1,21 @@
 package kiosk1;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("[ SHAKESHACK MENU ]\n" +
-                "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
-                "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
-                "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
-                "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
-                "0. 종료");
+        Scanner sc = new Scanner(System.in);
+        int input = -1;
+
+        while (input!=0) { // 0 입력 시 종료
+            System.out.println("""
+                    [ SHAKESHACK MENU ]
+                    1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거
+                    2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거
+                    3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거
+                    4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거
+                    0. 종료""");
+
+            System.out.println("- 입력: ");
+            input = sc.nextInt(); // ✅ 입력받기
+        }
     }
 }
