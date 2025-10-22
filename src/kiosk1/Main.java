ef6819269acff4e845a9e704c187561791654d98
minss22 <kms141516@gmail.com>
2025-10-22 10:51:11 +0900

    초기 커밋

--- src/kiosk1/Main.java
+++ src/kiosk1/Main.java
@@ -1,4 +1,12 @@
 package kiosk1;
 
 public class Main {
+    public static void main(String[] args) {
+        System.out.println("[ SHAKESHACK MENU ]\n" +
+                "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
+                "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
+                "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
+                "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
+                "0. 종료");
+    }
 }
