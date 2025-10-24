6daf960264807527fa4bd3e5855540d5996b5882
minss22 <kms141516@gmail.com>
2025-10-24 15:25:50 +0900

    ✨ feat: Menu 클래스에 MenuItem 출력 함수 추가
    - 반복문을 활용하여 순차적으로 MenuItem 출력

--- src/kiosk1/Menu.java
+++ src/kiosk1/Menu.java
@@ -3,13 +3,16 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Menu {
+    // 속성
     private String category;
     private List<MenuItem> menuItems = new ArrayList<>();
 
+    // 생성자
     Menu(String category) {
         this.category = category;
     }
 
+    // 기능
     public String getCategory() {
         return category;
     }
@@ -20,5 +23,14 @@ public class Menu {
 
     public void setMenuItems(MenuItem menuItem) {
         menuItems.add(menuItem);
+    }
+
+    public void printMenuItems() {
+        System.out.printf("\n[ %s MENU ]\n", category.toUpperCase());
+        for (MenuItem item : menuItems) {
+            int idx = menuItems.indexOf(item)+1;
+                System.out.printf("%d. %-13s | %4d 원 | %s\n", idx, item.getName(), item.getPrice(), item.getInfo());
+        }
+        System.out.print("0. 뒤로가기\n- 입력: ");
     }
 }
