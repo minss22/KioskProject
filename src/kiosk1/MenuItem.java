1a33f38e1c82952c1bf05bbaa10c670a26213ff6
minss22 <kms141516@gmail.com>
2025-10-24 15:44:21 +0900

    ✨ feat: MenuItem 클래스의 캡슐화
    - 필드에 직접 접근하지 못하도록 속성을 private으로 선언
    - 게터를 통해 데이터 관리

--- src/kiosk1/MenuItem.java
+++ src/kiosk1/MenuItem.java
@@ -1,13 +1,25 @@
 package kiosk1;
 
 public class MenuItem {
-    String name;
-    int price;
-    String info;
+    private String name;
+    private int price;
+    private String info;
 
     MenuItem(String name, int price, String info) {
         this.name = name;
         this.price = price;
         this.info = info;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public String getInfo() {
+        return info;
     }
 }
