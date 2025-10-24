9f67e2350eb7784968438dec6a41f6e00a8eb63d
minss22 <kms141516@gmail.com>
2025-10-24 16:23:05 +0900

    ✨ feat: Main 클래스 수정
    - Menu 객체 생성
    - Menu를 담는 List 선언 및 Menu 추가
    - MenuItem 객체 생성하면서 Menu 세터로 삽입
    - Kiosk 객체 생성 및 함수 호출

--- src/kiosk1/Main.java
+++ src/kiosk1/Main.java
@@ -4,14 +4,24 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        List<MenuItem> menuItems = new ArrayList<>();
-        // 햄버거 메뉴 세팅
-        menuItems.add(new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("Hamburger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거"));
+        // Menu 객체 생성을 통해 이름 설정
+        Menu burgers = new Menu("Burgers");
+        Menu Drinks = new Menu("Drinks");
+        Menu Desserts = new Menu("Desserts");
 
-        Kiosk kiosk = new  Kiosk(menuItems); // 객체 생성
+        // Menu List 선언 및 추가
+        List<Menu> menus = new ArrayList<>();
+        menus.add(burgers);
+        menus.add(Drinks);
+        menus.add(Desserts);
+
+        // 메뉴 세팅 - Menu 클래스 내 있는 List<MenuItem> 에 MenuItem 객체 생성하면서 삽입
+        burgers.setMenuItems(new MenuItem("ShackBurger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        burgers.setMenuItems(new MenuItem("SmokeShack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        burgers.setMenuItems(new MenuItem("Cheeseburger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        burgers.setMenuItems(new MenuItem("Hamburger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        Kiosk kiosk = new Kiosk(menus); // Kiosk 객체 생성
         kiosk.start();
     }
 }
