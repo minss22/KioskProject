2d7ba20024b319646b65efe4ef433bb8b1245342
minss22 <kms141516@gmail.com>
2025-10-26 02:22:45 +0900

    ♻ refactor: 메서드 생성
    - '메뉴 리스트 출력하는 기능'을 새로운 메서드로 분리
    - '입력 후 입력받은 숫자에 따라 다른 로직을 실행하는 기능'을 새로운 메서드로 분리

--- src/kiosk1/Kiosk.java
+++ src/kiosk1/Kiosk.java
@@ -12,42 +12,49 @@ public class Kiosk {
     }
 
     // 기능
-    public void start() { // 메서드: 입력 (반복)
-        Scanner sc = new Scanner(System.in);
-        int input;
+    public void start() { // 메뉴 관리 및 사용자 입력 처리 (반복)
         while (true) {
-            // ✅ 상위 카테고리 메뉴 리스트 출력
-            System.out.println("[ MAIN MENU ]");
-            for (Menu menu : menus) {
-                System.out.printf("%d. %s\n", menus.indexOf(menu)+1, menu.getCategory());
-            }
-            System.out.print("0. 종료\n- 입력: ");
+            /* 메뉴 카테고리 출력 후 입력받기 */
+            printMenus(); // ✅ 상위 카테고리 메뉴 리스트 출력
 
-            input = sc.nextInt(); // ✅ 입력받기
-            if (input==0) { // 0 입력 시 종료
-                System.out.println("\n프로그램을 종료합니다.");
-                break;
-            } else if (input < 0 || input > menus.size()) { // 유효하지 않은 입력에 대해 오류 메시지를 출력
-                System.out.println("⚠️ 선택하신 메뉴는 없습니다. 다시 입력해주세요.\n");
-                continue;
-            }
-            Menu menu = menus.get(input-1); // ✅ Menu 선택
+            int input1 = inputInt(menus); // ✅ 입력받기
+            if (input1 == 0) break;
+            else if (input1 == -1) continue;
 
-            // ✅ MenuItems 출력
-            menu.printMenuItems();
+            Menu menu = menus.get(input1 - 1); // ✅ Menu 선택
 
-            input = sc.nextInt(); // ✅ 입력받기
+            /* 메뉴 출력 후 입력받기 */
+            menu.printMenuItems(); // ✅ MenuItems 출력
             List<MenuItem> menuItems = menu.getMenuItems();
-            if (input==0) { // 0 입력 시 종료
-                System.out.println();
-                continue;
-            } else if (input < 0 ||input > menuItems.size()) { // 유효하지 않은 입력에 대해 오류 메시지를 출력
-                System.out.println("⚠️ 선택하신 메뉴는 없습니다. 다시 입력해주세요.\n");
-                continue;
-            }
 
-            MenuItem item = menuItems.get(input - 1); // ✅ MenuItem 선택 및 출력
+            int input2 = inputInt(menuItems); // ✅ 입력받기
+            if (input2 == -1) continue;
+
+            MenuItem item = menuItems.get(input2 - 1); // ✅ MenuItem 선택 및 출력
             System.out.printf("\n# 선택한 메뉴: %s(%d원) - %s\n\n", item.getName(), item.getPrice(), item.getInfo());
         }
+    }
+
+    private void printMenus() {
+        System.out.println("[ MAIN MENU ]");
+        for (Menu menu : menus) {
+            System.out.printf("%d. %s\n", menus.indexOf(menu) + 1, menu.getCategory());
+        }
+        System.out.print("0. 종료\n- 입력: ");
+    }
+
+    private <S> int inputInt(List<S> list) {
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+        if (input == 0) { // 0 입력 시 종료
+            System.out.println();
+            if (list == menus) System.out.println("프로그램을 종료합니다.");
+            else return -1;
+        } else if (input < 0 || input > list.size()) { // 유효하지 않은 입력에 대해 오류 메시지를 출력
+            System.out.println("⚠️ 선택하신 메뉴는 없습니다. 다시 입력해주세요.\n");
+            return -1;
+        }
+        return input;
     }
 }
